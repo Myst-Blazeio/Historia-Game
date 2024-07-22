a22fd21340893c68f729f2fb168c4211416b10da
Pushkar Pan <143222664+Myst-Blazeio@users.noreply.github.com>
2024-07-23 01:18:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,27 @@
+  const events = [
 
-        const events = [
-    { event: "Invention of the Wheel", date: -3500, image: "img1.png", description: "The invention of the wheel, a key technological advancement." },
-    { event: "Discovery of Fire", date: -100000, image: "img1.png", description: "The discovery and control of fire." },
-    { event: "First Manned Moon Landing", date: 1969, image: "img1.png", description: "Neil Armstrong and Buzz Aldrin land on the moon." },
-    { event: "Fall of the Berlin Wall", date: 1989, image: "img1.png", description: "The fall of the Berlin Wall marking the end of the Cold War." },
-    { event: "Start of World War I", date: 1914, image: "img1.png", description: "The beginning of World War I." },
-    { event: "Printing Press Invented", date: 1440, image: "img1.png", description: "Johannes Gutenberg invents the printing press." },
-    { event: "Signing of the Declaration of Independence", date: 1776, image: "img1.png", description: "The United States Declaration of Independence is signed." },
-    { event: "French Revolution Begins", date: 1789, image: "img1.png", description: "The French Revolution starts." },
-    { event: "First Flight by the Wright Brothers", date: 1903, image: "img1.png", description: "The Wright brothers make their first successful flight." },
-    { event: "Introduction of the Internet", date: 1983, image: "img1.png", description: "The ARPANET adopts TCP/IP protocol, marking the beginning of the internet." },
-    { event: "Renaissance Period", date: 1400, image: "img1.png", description: "The Renaissance period begins in Europe." },
-    { event: "Columbus Discovers America", date: 1492, image: "img1.png", description: "Christopher Columbus discovers America." },
-    { event: "Industrial Revolution", date: 1760, image: "img1.png", description: "The Industrial Revolution begins." },
-    { event: "End of World War II", date: 1945, image: "img1.png", description: "World War II comes to an end." },
-    { event: "Start of the Cold War", date: 1947, image: "img1.png", description: "The Cold War begins." },
-    { event: "Invention of the Telephone", date: 1876, image: "img1.png", description: "Alexander Graham Bell invents the telephone." },
-    { event: "The Great Depression", date: 1929, image: "img1.png", description: "The Great Depression begins." },
-    { event: "Nelson Mandela's Release from Prison", date: 1990, image: "img1.png", description: "Nelson Mandela is released from prison." },
-    { event: "Fall of Constantinople", date: 1453, image: "img1.png", description: "The fall of Constantinople marks the end of the Byzantine Empire." },
-    { event: "Start of the Black Death", date: 1347, image: "img1.png", description: "The Black Death pandemic begins in Europe." },
+    { event: "Invention of the Wheel", date: -3500, image: "img.png", description: "The invention of the wheel, a key technological advancement." },
+    { event: "Discovery of Fire", date: -100000, image: "img.png", description: "The discovery and control of fire." },
+    { event: "First Manned Moon Landing", date: 1969, image: "img.png", description: "Neil Armstrong and Buzz Aldrin land on the moon." },
+    { event: "Fall of the Berlin Wall", date: 1989, image: "img.png", description: "The fall of the Berlin Wall marking the end of the Cold War." },
+    { event: "Start of World War I", date: 1914, image: "img.png", description: "The beginning of World War I." },
+    { event: "Printing Press Invented", date: 1440, image: "img.png", description: "Johannes Gutenberg invents the printing press." },
+    { event: "Signing of the Declaration of Independence", date: 1776, image: "img.png", description: "The United States Declaration of Independence is signed." },
+    { event: "French Revolution Begins", date: 1789, image: "img.png", description: "The French Revolution starts." },
+    { event: "First Flight by the Wright Brothers", date: 1903, image: "img.png", description: "The Wright brothers make their first successful flight." },
+    { event: "Introduction of the Internet", date: 1983, image: "img.png", description: "The ARPANET adopts TCP/IP protocol, marking the beginning of the internet." },
+    { event: "Renaissance Period", date: 1400, image: "img.png", description: "The Renaissance period begins in Europe." },
+    { event: "Columbus Discovers America", date: 1492, image: "img.png", description: "Christopher Columbus discovers America." },
+    { event: "Industrial Revolution", date: 1760, image: "img.png", description: "The Industrial Revolution begins." },
+    { event: "End of World War II", date: 1945, image: "img.png", description: "World War II comes to an end." },
+    { event: "Start of the Cold War", date: 1947, image: "img.png", description: "The Cold War begins." },
+    { event: "Invention of the Telephone", date: 1876, image: "img.png", description: "Alexander Graham Bell invents the telephone." },
+    { event: "The Great Depression", date: 1929, image: "img.png", description: "The Great Depression begins." },
+    { event: "Nelson Mandela's Release from Prison", date: 1990, image: "img.png", description: "Nelson Mandela is released from prison." },
+    { event: "Fall of Constantinople", date: 1453, image: "img.png", description: "The fall of Constantinople marks the end of the Byzantine Empire." },
+    { event: "Start of the Black Death", date: 1347, image: "img.png", description: "The Black Death pandemic begins in Europe." },
 
 ];
-
-//        const events = [
-//    { event: "Invention of the Wheel", date: 2000, image: "wheel.jpg", description: "The invention of the wheel, a key technological advancement." },
-//     { event: "Discovery of Fire", date: 2000, image: "fire.jpg", description: "The discovery and control of fire." },
-//     { event: "First Manned Moon Landing", date: 2000, image: "moon_landing.jpg", description: "Neil Armstrong and Buzz Aldrin land on the moon." },
-//     { event: "Fall of the Berlin Wall", date:2000, image: "berlin_wall.jpg", description: "The fall of the Berlin Wall marking the end of the Cold War." },
-//     { event: "Start of World War I", date: 2000, image: "ww1.jpg", description: "The beginning of World War I." },
-//     { event: "Printing Press Invented", date: 2000, image: "printing_press.jpg", description: "Johannes Gutenberg invents the printing press." },
-//     { event: "Signing of the Declaration of Independence", date: 2000, image: "declaration.jpg", description: "The United States Declaration of Independence is signed." },
-//     { event: "French Revolution Begins", date: 2000, image: "french_revolution.jpg", description: "The French Revolution starts." },
-//     { event: "First Flight by the Wright Brothers", date: 2000, image: "wright_brothers.jpg", description: "The Wright brothers make their first successful flight." },
-//     { event: "Introduction of the Internet", date: 2000, image: "internet.jpg", description: "The ARPANET adopts TCP/IP protocol, marking the beginning of the internet." },
-//     { event: "Renaissance Period", date: 2000, image: "renaissance.jpg", description: "The Renaissance period begins in Europe." },
-//     { event: "Columbus Discovers America", date: 2000, image: "columbus.jpg", description: "Christopher Columbus discovers America." },
-//     { event: "Industrial Revolution", date: 2000, image: "industrial_revolution.jpg", description: "The Industrial Revolution begins." },
-//     { event: "End of World War II", date: 2000, image: "ww2_end.jpg", description: "World War II comes to an end." },
-//     { event: "Start of the Cold War", date: 2000, image: "cold_war.jpg", description: "The Cold War begins." },
-//     { event: "Invention of the Telephone", date: 2000, image: "telephone.jpg", description: "Alexander Graham Bell invents the telephone." },
-//     { event: "The Great Depression", date: 2000, image: "great_depression.jpg", description: "The Great Depression begins." },
-//     { event: "Nelson Mandela's Release from Prison", date: 2000, image: "mandela.jpg", description: "Nelson Mandela is released from prison." },
-//     { event: "Fall of Constantinople", date: 2000, image: "constantinople.jpg", description: "The fall of Constantinople marks the end of the Byzantine Empire." },
-//     { event: "Start of the Black Death", date: 2000, image: "black_death.jpg", description: "The Black Death pandemic begins in Europe." },
-
-// ];
-
 
 let mistakes = 0;
 let currentStreak = 0;
@@ -86,6 +61,7 @@ function initializeGame() {
     const baseCard = createCard(baseEvent);
     baseCard.querySelector('.date').style.display = 'block';
     baseCard.classList.add('correct');
+    baseCard.classList.add('flippable');
     baseCard.draggable = false;
     timeline.appendChild(baseCard);
 
@@ -97,14 +73,20 @@ function createCard(event) {
     card.className = 'card';
     card.draggable = true;
     card.innerHTML = `
-        <div class="name">${event.event}</div>
-        <img src="${event.image}" alt="${event.event}" style="width: 100px;">
-        <div class="description">${event.description}</div>
-        <div class="date">${event.date}</div>
+        
+            <div class="name">${event.event}</div>
+            <img src="${event.image}" alt="${event.event}" style="width: 100px;">
+            <div class="description">${event.description}</div>
+            <div class="date">${event.date}</div>
+        
+        <div class="back">
+        <p>wikipedia link</p>
+        </div>
     `;
     card.dataset.date = event.date;
     card.addEventListener('dragstart', dragStart);
     card.addEventListener('dragend', dragEnd);
+    card.addEventListener('click', flipCard);
     return card;
 }
 
@@ -154,16 +136,20 @@ timeline.addEventListener('drop', e => {
     const card = document.querySelector(`.card[data-date='${date}']`);
     card.querySelector('.date').style.display = 'block';
     card.draggable = false;
+    
+    card.classList.add('flippable');
+    card.addEventListener('click', flipCard);
 
-     if (currentCardContainer.children.length === 0 && timeline.children.length === events.length) {
-            message.textContent = `Congratulations! You've placed all cards correctly.`;
-            nextCardButton.disabled = true;
-        } else {
-            if (currentCardContainer.children.length === 0) {
-                nextCardButton.disabled = false;
-            }
+    if (currentCardContainer.children.length === 0 && timeline.children.length === events.length) {
+        message.textContent = `Congratulations! You've placed all cards correctly.`;
+        gameOver();
+        nextCardButton.disabled = true;
+    } else {
+        if (currentCardContainer.children.length === 0) {
+            nextCardButton.disabled = false;
         }
-      if (checkOrder()) {
+    }
+    if (checkOrder()) {
         card.classList.add('correct');
         currentCardContainer.removeChild(card);
     } else {
@@ -202,21 +188,24 @@ function checkOrder() {
     return true;
 }
 
-
 function gameOver() {
-        document.getElementById("current-card-container").style.display="none";
-        document.getElementById("next-card-button").style.display="none";
-        document.getElementById("timeline").style.display="none";
+    document.getElementById("current-card-container").style.display = "none";
+    document.getElementById("next-card-button").style.display = "none";
+    document.getElementById("timeline").style.display = "none";
     if (timeline.children.length === events.length && checkOrder()) {
         message.textContent = `Congratulations! You've placed all cards correctly.`;
-
     } else {
         message.textContent = `Game Over! Longest streak: ${longestStreak}`;
-        
     }
     timeline.innerHTML = '';
     currentCardContainer.innerHTML = '';
     nextCardButton.disabled = true;
+}
+
+function flipCard(e) {
+    if (e.currentTarget.classList.contains('flippable')) {
+        e.currentTarget.classList.toggle('flipped');
+    }
 }
 
 initializeGame();
